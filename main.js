c99872be5fb2706c253bfba61aecdcc9c91737d9
tom-mindlab <tom@themindlab.co.uk>
2018-07-23 15:13:52 +0100

    closer

--- main.js
+++ main.js
@@ -3,11 +3,15 @@ import { screen, utils, controls } from 'wombat';
 import template from './visual-search-t.html';
 import languages from './lang.json';
 
-import { Product, Shelf, ShelfRack } from './shelf_rack';
-import { $buildDOM } from './shelf_rack';
+import { ShelfRack } from './shelf_rack';
+import { $newLayout } from './shelf_rack';
+
+function getJSONAsync(URI) {
+	return new Promise(res => $.getJSON(URI, res));
+}
 
 function onClick($element, click_info) {
-	return new Promise(function(resolve) {
+	return new Promise(function (resolve) {
 		$element.on('click', e => {
 			if (typeof click_info === 'object') {
 				click_info.m_pos.x = e.pageX;
@@ -33,47 +37,6 @@ async function showScreen($DOM, replacements) {
 }
 
 // //////////////////////////////////////////////////////////////
-// generate new DOM for generated shelf rack                   //
-// //////////////////////////////////////////////////////////////
-// args:
-// 		[0]: $DOM	:	object (jQuery element)
-//				the stimuli div element
-//		[1]: product_scale	:	number
-//				the scale of products on the shelf
-// return:
-// 		object (jQuery element)
-// desc:
-//		generates a DOM for a given shelf rack layout, using the input $DOM as a base
-//		returns a completed DOM in the <div class="rack"><div class="shelf ...">... style
-async function $newLayout($container_DOM, product_scale, rack) {
-	let $rack_DOM = $container_DOM;
-	for (let item of rack.items) {
-		$rack_DOM.append(await $buildDOM(item));
-	}
-
-	//$rack_DOM.append($rack_DOM);
-	//$rack_DOM.show();
-
-	//$rack_DOM.find('.shelf').css('height', 100 / $rack_DOM.children().length + '%');
-
-	$rack_DOM.find('.product').each(function() {
-		$(this).css(
-			'background-size',
-			'auto ' +
-				($(this)
-					.parent()
-					.height() *
-					product_scale *
-					100) /
-					100 +
-				'px'
-		);
-	});
-
-	return $rack_DOM;
-}
-
-// //////////////////////////////////////////////////////////////
 // MAIN SCRIPT LOOP                                            //
 // //////////////////////////////////////////////////////////////
 // args:
@@ -86,24 +49,17 @@ async function $newLayout($container_DOM, product_scale, rack) {
 // 		the main stimuli display and input recording loop, generates shelves on the fly from the config
 // 		and records user input (which it returns as an array)
 async function main($DOM, configuration, pause, pause_replacements) {
-	let rack = new ShelfRack(configuration.layout, configuration.item_classes, configuration.product);
+	let rack = new ShelfRack(configuration.layout, configuration.item_classes, configuration.product_info);
 	let click_data = [];
 
 	let timer = controls.timer($DOM.find('.timer'));
-	timer.duration(configuration.timer_duration);
-	let reset_duration = 500;
-	timer.resetDuration(reset_duration);
-	timer.timeout(async function() {
+	timer.duration(configuration.timer.duration);
+	timer.resetDuration(configuration.timer.reset_duration);
+	timer.timeout(async function () {
 		timer.stop();
-		$DOM.fadeOut(reset_duration);
+		$DOM.fadeOut(configuration.timer.reset_duration / 2);
 		await showScreen(pause, pause_replacements);
-		timer.reset();
-		await new Promise(res =>
-			setTimeout(() => {
-				$DOM.fadeIn(reset_duration);
-				res();
-			}, reset_duration)
-		);
+		await Promise.all([timer.reset(), $DOM.fadeIn(configuration.timer.reset_duration).promise()]);
 		timer.start();
 	});
 
@@ -111,20 +67,16 @@ async function main($DOM, configuration, pause, pause_replacements) {
 	let $stimuli = $DOM.find('.stimuli');
 	let $instruction = $DOM.find('.instruction');
 	for (let i = 0; i < configuration.repeats; ++i) {
-		$instruction.hide();
 		rack.populateShelves();
-		$stimuli.empty();
-		$stimuli.append(await $newLayout($stimuli, configuration.product.scale, rack));
+
+		$stimuli.append(await $newLayout($stimuli, configuration.product_info.scale, rack));
 		$stimuli.hide();
+
+		// abstract this into the config
 		let requested_product = rack.product_classes[Math.floor(Math.random() * rack.product_classes.length)].name;
 		$instruction.text('Please click on the ' + requested_product);
-		await new Promise(res =>
-			setTimeout(() => {
-				$stimuli.fadeIn(reset_duration);
-				$instruction.fadeIn(reset_duration);
-				res();
-			}, reset_duration)
-		);
+		$stimuli.fadeIn(configuration.timer.reset_duration);
+		$instruction.fadeIn(configuration.timer.reset_duration);
 		timer.start();
 		let click_info = {
 			m_pos: {
@@ -132,7 +84,7 @@ async function main($DOM, configuration, pause, pause_replacements) {
 				y: NaN
 			},
 			product_type: {
-				requested: rack.product_classes[Math.floor(Math.random() * rack.product_classes.length)].name,
+				requested: requested_product,
 				clicked: null
 			},
 			time_taken: NaN
@@ -141,7 +93,9 @@ async function main($DOM, configuration, pause, pause_replacements) {
 		timer.stop();
 		click_info.time_taken = timer.value();
 		click_data.push(click_info);
-		timer.reset();
+		await timer.reset();
+		$instruction.empty();
+		$stimuli.empty();
 	}
 
 	return click_data;
@@ -159,7 +113,7 @@ async function main($DOM, configuration, pause, pause_replacements) {
 //			 the meta data which will be written to the user's session objects (maintained between elements)
 //		[1]: data		:	array
 //			 the data produced by the user running through the element
-export default async function(configuration, callback) {
+export default async function (configuration, callback) {
 	// language
 	let lang = utils.buildLanguage(languages, configuration);
 
@@ -175,7 +129,7 @@ export default async function(configuration, callback) {
 	let meta = {};
 	let data = await main($main, configuration, $pause_screen, lang.screens.pause);
 
-	screen.exit('fade', async function() {
+	screen.exit('fade', async function () {
 		callback(meta, data);
 	});
 }
--- shelf_rack.js
+++ shelf_rack.js
@@ -1,3 +1,6 @@
+import ldShuffle from 'lodash/shuffle'
+
+
 class Item {
 	constructor(name, URI, shelf_proportion) {
 		this.name = name;
@@ -6,7 +9,7 @@ class Item {
 	}
 }
 
-export class Product extends Item {
+class Product extends Item {
 	constructor(json_product_obj) {
 		super(json_product_obj.name, json_product_obj.URI, 1);
 	}
@@ -15,21 +18,22 @@ export class Product extends Item {
 // TODO:
 // all of these
 const OVERFLOW_CALLBACKS = {
-	SQUASH: function() {},
+	SQUASH: function () { },
 	OVERWRITE: {
-		LEFT: function() {},
-		RIGHT: function() {}
+		LEFT: function () { },
+		RIGHT: function () { }
 	},
-	FAIL: function() {}
+	FAIL: function () { }
 };
 
-export class Shelf extends Item {
+class Shelf extends Item {
 	constructor(json_shelf_obj, overflow_callback) {
+
 		super(json_shelf_obj.name, json_shelf_obj.URI, 1);
 		this.overflow_callback = overflow_callback;
 		this.pack_from = json_shelf_obj.pack_from;
+		this.bounds = json_shelf_obj.bounds;
 		this.items = [];
-		this.constraints = json_shelf_obj.constraints;
 	}
 
 	pop() {
@@ -43,11 +47,12 @@ export class Shelf extends Item {
 			// TODO:
 			// 1.   check if we have room for another product (width measurement), invoke overflow callback if there's no room
 			// 1.1  this could potentially be deferred until rendering
-			if (this.items.length < this.constraints.max) {
-				this.items.push(item);
-			} else {
-				throw new RangeError('shelf cannot accept more items (max of "' + this.constraints.max + '" already reached)');
-			}
+			// if (this.items.length < this.constraints.max) {
+			// 	this.items.push(item);
+			// } else {
+			// 	throw new RangeError('shelf cannot accept more items (max of "' + this.constraints.max + '" already reached)');
+			// }
+			this.items.push(item);
 		}
 	}
 
@@ -55,129 +60,89 @@ export class Shelf extends Item {
 		this.items.splice(0, this.items.length);
 	}
 
-	capacity() {
-		let capacity = this.constraints.max || 0;
-		for (let item of this.items) {
-			if (item instanceof Shelf) {
-				capacity += item.capacity();
-			}
-		}
-		return capacity;
-	}
 }
 
 export class ShelfRack {
-	constructor(layout_arr, item_type_arr, product_obj) {
+	constructor(layout_arr, item_classes, product_obj) {
+
+
+		// holy fuck this is bad code, needs a refactor (change in the config structure)... cba right now (16/07/18) [tom]
+		this.product_classes = item_classes.products;
+
+		this.shelf_classes = item_classes.shelves;
+
 		this.items = [];
-		this.items = parseItems(layout_arr, this.items, item_type_arr);
+		this.items = parseItems(layout_arr, this.items, this.shelf_classes);
 		this.product_info = product_obj;
-
-		// holy fuck this is bad code, needs a refactor (change in the config structure)... cba right now (16/07/18)
-		this.product_classes = (() => {
-			let arr = [];
-			for (let item_type of item_type_arr) {
-				if (item_type.type === 'product') {
-					arr.push(item_type);
-				}
-			}
-			return arr;
-		})();
-
-		this.shelf_classes = (() => {
-			let arr = [];
-			for (let item_type of item_type_arr) {
-				if (item_type.type === 'shelf') {
-					arr.push(item_type);
-				}
-			}
-			return arr;
-		})();
-
-		if (this.product_info.count > rack_capacity(this.items)) {
-			throw new RangeError(
-				'Shelf rack capacity (' +
-					rack_capacity(this.items) +
-					') cannot contain required amount of products (' +
-					this.product_info.count +
-					')'
-			);
-		}
-
-		if (this.items.length > 0) {
-			for (let index in this.items) {
-				if (!(this.items[index] instanceof Shelf)) {
-					throw new TypeError('ShelfRack top level must consist only of Shelf objects');
-				}
-			}
-		}
 	}
 
+	// yields a product object
+	// this generator will yield the same product from the list of products N times where
+	//		N = (target count of products) / (number of product types)
+	// e.g:
+	//		N = 20 / 4
+	//		  = 5
+	// (so N is just the size of the group for each product).
+	// After yielding the same product N times, it will then yield the next product in the list N times. After this, it will loop from the start.
+	// If
+	//		(target count of products) % (number of product types) != 0			(not cleanly divisible)
+	// then the remainder of that division is captured.
+	// The size of N for the products (starting at the back, moving up the array) in the product list will be increased by 1 and the
+	// remainder of the division from earlier will be decreased by 1 until the remainder is zero.
+	// By doing this, the leftover amount is spread as evenly as possible across the product list.
 	*genProduct() {
-		let i = Math.floor(Math.random() * this.product_classes.length);
-		let group_size = this.product_info.count / (this.product_classes.length * this.items.length);
+		let group_sizes = new Array(this.product_classes.length);
+		group_sizes.fill(Math.floor(this.product_info.count / this.product_classes.length));
+		for (let remainder = this.product_info.count - group_sizes.reduce((s, v) => { return s + v }); remainder > 0; --remainder) {
+			group_sizes[remainder]++;
+		}
+
 		while (true) {
-			if (i >= this.product_classes.length) {
-				i = 0;
+			for (let i = 0; i < this.product_classes.length; ++i) {
+				for (let j = 0; j < group_sizes[i]; ++j) {
+					yield this.product_classes[i];
+				}
 			}
-			for (let j = 0; j < group_size * this.product_classes[i].bias; ++j) {
-				yield this.product_classes[i];
-			}
-			i++;
 		}
 	}
 
 	populateShelves() {
-		for (let shelf of this.items) {
-			if (shelf instanceof Shelf) {
-				shelf.clear();
-				// todo:
-				// instead of randomising, utilise product_info.count?
-				let target = Math.floor(Math.random() * (shelf.constraints.max - shelf.constraints.min + 1)) + shelf.constraints.min;
-				let gen = this.genProduct();
-				while (shelf.items.length < target) {
-					shelf.push(new Product(gen.next().value));
+		let gen = this.genProduct();
+		let targets = new Array(this.items.length);
+		targets.fill(Math.floor(this.product_info.count / this.items.length));
+		for (let i = 0; targets.reduce((s, v) => { return s + v }) < this.product_info.count; ++i) {
+			++targets[i];
+		}
+
+		for (let shelf in this.items) {
+
+			if (this.items[shelf] instanceof Shelf) {
+				this.items[shelf].clear();
+
+				while (this.items[shelf].items.length < targets[shelf]) {
+					this.items[shelf].push(new Product(gen.next().value));
 				}
 			}
 		}
+
+		this.product_classes = ldShuffle(this.product_classes);
 	}
 }
 
-// why isnt this a member function?? dont remember
-function rack_capacity(shelf_rack_items) {
-	let cap = 0;
-	for (let item in shelf_rack_items) {
-		cap += shelf_rack_items[item].capacity();
-	}
-	return cap;
-}
 
-function parseItems(json_obj, item_arr, item_types_arr) {
+function parseItems(json_obj, item_arr, shelf_types_arr) {
 	if (!Array.isArray(item_arr)) {
 		throw new TypeError('parseItems requires an array-type object to build against');
 	}
 	if (Array.isArray(json_obj)) {
 		for (let i = 0; i < json_obj.length; ++i) {
-			item_arr = parseItems(json_obj[i], item_arr, item_types_arr);
+			item_arr = parseItems(json_obj[i], item_arr, shelf_types_arr);
 		}
 	} else {
-		json_obj = Object.assign(json_obj, item_types_arr.find(item => item.name === json_obj.name));
+		json_obj = Object.assign(json_obj, shelf_types_arr.find(item => item.name === json_obj.name));
 
-		if (json_obj.type === 'shelf') {
-			item_arr.push(new Shelf(json_obj, OVERFLOW_CALLBACKS.FAIL));
-		} else if (json_obj.type === 'product') {
-			if (!json_obj.hasOwnProperty('bias')) {
-				json_obj.bias = 1;
-			}
-			item_arr.push(new Product(json_obj));
-		} else {
-			throw new Error('parsed json object with invalid "type" property: ' + json_obj.type);
-		}
-		// here we scan for nested items
-		for (let key in json_obj) {
-			if (Array.isArray(json_obj[key])) {
-				item_arr[item_arr.length - 1].items = parseItems(json_obj[key], item_arr[item_arr.length - 1].items, item_types_arr);
-			}
-		}
+
+		item_arr.push(new Shelf(json_obj, OVERFLOW_CALLBACKS.FAIL));
 	}
 	return item_arr;
 }
@@ -189,9 +154,14 @@ async function $asElement(item) {
 	$DOM.css('flex-grow', item.shelf_proportion);
 
 	if (item instanceof Product) {
+		// $DOM = $('<div></div>');
+		// $DOM.append($('<img src="' + item.URI + '">'));
+		// $DOM = $('<img src="' + item.URI + '">');
 		$DOM.addClass('product ' + item.name);
 	} else if (item instanceof Shelf) {
 		$DOM.css('flex-direction', item.pack_from);
+		$DOM.css('padding-top', item.bounds.top);
+		$DOM.css('padding-bottom', item.bounds.bottom);
 		$DOM.addClass('shelf ' + item.name);
 	} else {
 		throw new TypeError('Expected shelf rack item');
@@ -208,4 +178,32 @@ export async function $buildDOM(item) {
 		}
 	}
 	return $DOM;
+}
+
+// //////////////////////////////////////////////////////////////
+// generate new DOM for generated shelf rack                   //
+// //////////////////////////////////////////////////////////////
+// args:
+// 		[0]: $DOM	:	object (jQuery element)
+//				the stimuli div element
+//		[1]: product_scale	:	number
+//				the scale of products on the shelf
+// return:
+// 		object (jQuery element)
+// desc:
+//		generates a DOM for a given shelf rack layout, using the input $DOM as a base
+//		returns a completed DOM in the <div class="rack"><div class="shelf ...">... style
+export async function $newLayout($container_DOM, product_scale, rack) {
+	let $rack_DOM = $container_DOM;
+	for (let item of rack.items) {
+		$rack_DOM.append(await $buildDOM(item));
+	}
+
+	$rack_DOM.find('.shelf').css('height', 100 / rack.items.length + '%');
+	$rack_DOM.find('.product').each(function () {
+		$(this).css('width', '100%');
+		$(this).css('margin-top', ((100 - (product_scale * 100)) * $(this).height()) / 100 + 'px');
+	});
+
+	return $rack_DOM;
 }
